23da49ed93a24639251387cabd8a2a4b8c41d295
Phluso <pedrolusocosta@gmail.com>
2024-08-31 22:37:14 -0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let images = document.querySelectorAll(".image");
+
+images.forEach(img => {                    //verifica se houve um clique em alguma imagem
+    img.addEventListener("click", () =>{   //se houver um clique
+        window.location = img.src;         //abrir a fonte da imagem
+    })
+})
+const background = document.getElementById("background");
+window.addEventListener("scroll", e=>{
+    background.style.opacity = 1 - (window.scrollY / 700);
+    background.style.objectPosition = `50% ${80 - window.scrollY / 5}%`;
+})
